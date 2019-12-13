1976da07f028cfaffc8aa0ac85bf0d13bf9365f6
Arik Rinberg <Arik.Rinberg@campus.technion.ac.il>
2019-12-13 09:03:41 +0200

    Altered script to skip lock based as a default. To run with, use withLockBased parameter

--- run_test.py
+++ run_test.py
@@ -10,10 +10,12 @@ CMD = 'java -cp "./*" com.yahoo.sketches.characterization.Job {}'
 TESTS = ["figure_1", "figure_6_a", "figure_6_b", "figure_7", "figure_8", "figure_9", "table_2"]
 
 
-def createTest(testName):
+def createTest(testName, withLockBased = False):
     path = join("conf_files", testName)
     confFiles = [join(path, f) for f in listdir(path) if f.endswith(".conf")]
-    print(confFiles)
+    if not withLockBased:
+      confFiles = [f for f in confFiles if "LockBased" not in f]
+    
     for confFile in confFiles:
         subprocess.run(CMD.format(confFile), shell=True)
     
@@ -24,4 +26,8 @@ if __name__ == "__main__":
         print("USAGE: ", sys.argv[0], TESTS)
         exit(1)
     
-    createTest(sys.argv[1])
+    withLockBased = False;
+    if len(sys.argv) >= 3:
+      withLockBased = sys.argv[2] == "withLockBased"
+        
+    createTest(sys.argv[1], withLockBased)
